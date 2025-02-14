55952e4693d548c1964dd1c37abbe994278ec1b9
worthyag <worthydevelops@gmail.com>
2025-02-14 15:31:46 +0000

    Put the API key in a safe space.

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
 
+import os
+# Importing the necessary functions from the dotenv library.
+from dotenv import load_dotenv, dotenv_values
+
+# Loading variables from the .env file.
+load_dotenv()
+
 app = Flask("SRE Helper")
+
+print(os.getenv("SRE_HELPER_GEMINI_API_KEY"))
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    response = "Lisa"
+    return render_template("index.html", res=response)
 
 
 app.run(host="0.0.0.0", port=5002)
