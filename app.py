f55afc8c778553588f0602edcc558f035a304c46
worthyag <worthydevelops@gmail.com>
2025-02-17 09:04:02 +0000

    Completed db integration.

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request
+from flask import *
 
 import os
 from google import genai
 from google.genai import types
+
+import pymysql
+import traceback
+import pymysql.cursors
 
 # genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
@@ -14,6 +18,59 @@ prompts = [
 ]
 
 app = Flask("SRE Helper")
+
+# app.config["MYSQL_HOST"] = "192.168.2.136"
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "sreUser"
+app.config["MYSQL_PASSWORD"] = "password12-"
+app.config["MYSQL_DB"] = "sreHelper"
+app.config["MYSQL_PORT"] = 3306
+
+# Database Configuration
+db_config = {
+    "host": app.config["MYSQL_HOST"],
+    "user": app.config["MYSQL_USER"],
+    "password": app.config["MYSQL_PASSWORD"],
+    "database": app.config["MYSQL_DB"],
+    "port": app.config["MYSQL_PORT"]
+}
+
+
+def get_db_connection():
+    # Returning the rows as dictionaries to make them easier to work with.
+    return pymysql.connect(**db_config, cursorclass=pymysql.cursors.DictCursor)
+
+
+def create_table(cursor):
+    # Selecting the database.
+    cursor.execute("USE sreHelper")
+
+    # Creating the table.
+    cursor.execute("""CREATE TABLE IF NOT EXISTS library (
+                    book_id INT AUTO_INCREMENT PRIMARY KEY,
+                    book_name VARCHAR(500) NOT NULL,
+                    book_author VARCHAR(500) NOT NULL,
+                    book_desc VARCHAR(2000)
+                   )""")
+    return cursor
+
+
+def start_db_connection():
+    with get_db_connection() as connection:
+        with connection.cursor() as cursor:
+            create_table(cursor)
+            # Making sure that the changes persist.
+            connection.commit()
+    # Returning a new connection for further use.
+    return get_db_connection()
+
+
+def get_error(e, msg=None):
+    error_message = str(e)
+    error_trace = traceback.format_exc()
+
+    msg = "Error occurred." if msg is None else msg
+    return jsonify({"error": f"{msg}\nError msg: {error_message}\nError trace: {error_trace}"}), 500
 
 
 @app.route("/")
@@ -59,12 +116,88 @@ def advice():
     return render_template("advice.html", res=cleaned_response)
 
 
-@app.route("/library")
+@app.route("/library", methods=['GET'])
 def viewLibrary():
+    try:
+        with get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                # Selecting all the books.
+                cursor.execute("SELECT * FROM library")
+
+                # Fetching all the results.
+                all_books = cursor.fetchall()
+
+                if all_books:
+                    return render_template("library.html", books=all_books)
+                return render_template("emptyLibrary.html")
+
+    except Exception as e:
+        return get_error(e)
     return render_template("library.html")
 
 
+@app.route("/library/add/")
+def addBook():
+    return render_template("addBook.html")
+
+
+@app.route("/library/view/", methods=['POST'])
+def viewBook():
+    book_id = request.form.get("book-id")
+
+    try:
+        with get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                # Selecting a specific book.
+                sql = "SELECT * FROM library WHERE book_id = %s"
+                cursor.execute(sql, (book_id,))
+                selected_book = cursor.fetchone()
+
+                if selected_book:
+                    return render_template("viewBook.html", book=selected_book)
+                else:
+                    return jsonify({"message": f"Book not found {book_id}"}), 404
+    except Exception as e:
+        return get_error(e)
+
+
+@app.route("/library/search/<id>/", methods=['GET'])
+def getBook(id):
+    try:
+        with get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM library WHERE book_id = %s"
+                cursor.execute(sql, (id,))
+                selected_book = cursor.fetchone()
+
+                if selected_book:
+                    return render_template("viewBook.html", movie=selected_book)
+                else:
+                    return jsonify({"message": "Book not found"}), 404
+    except Exception as e:
+        return get_error(e)
+
+
+@app.route("/library/save/", methods=["POST"])
+def addedBook():
+    book_name = request.form.get("book-name")
+    book_author = request.form.get("book-author")
+    book_desc = request.form.get("book-desc")
+
+    try:
+        with get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO library (book_name, book_author, book_desc) VALUES (%s, %s, %s)"
+                cursor.execute(sql, (book_name, book_author, book_desc))
+                # Committing the transaction.
+                connection.commit()
+                return render_template("saved.html", book_name=book_name, book_author=book_author, book_desc=book_desc)
+    except Exception as e:
+        return get_error(e, "Error occurred when adding book. Book not added.")
+
+
+# Creating the table
+start_db_connection()
 app.run(host="0.0.0.0", port=5002)
 
-#  .lstrip("```html\n")).rstrip("\n```"))
 # mimetypes are `text/plain`, `application/json` and `text/x.enum
